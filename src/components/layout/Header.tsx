886e56ebd897584ffb163e7c39201270f1860079
RomaLetodiani <roman.letodiani03@gmail.com>
2024-01-19 13:03:08 +0400

    built header component

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,5 +1,34 @@
+import logo from '../../assets/images/logo.svg';
+import SectionWrapper from '../SectionWrapper';
+import { textObj } from '../shared/consts';
+
 const Header = () => {
-  return <div>Header</div>;
+  const { header } = textObj;
+  return (
+    <SectionWrapper>
+      <div className="flex justify-between items-center gap-5 sm:gap-10 font-raleWay py-8 sm:py-16">
+        <div>
+          <img
+            className="min-w-[70px] w-[100px] sm:w-[150px]"
+            src={logo}
+            alt="Fylo-Logo"
+          />
+        </div>
+        <div>
+          <ul className="flex gap-5 sm:gap-10">
+            {header.map((item, index) => (
+              <li
+                className="text-very-dark-blue text-xs sm:text-xl cursor-pointer hover:text-bright-blue"
+                key={index}
+              >
+                {item}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </SectionWrapper>
+  );
 };
 
 export default Header;

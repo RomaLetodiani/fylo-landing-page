a6f0adac2cac1b6d1da5ba525ec729c23f845b5c
RomaLetodiani <roman.letodiani03@gmail.com>
2024-02-01 00:10:03 +0400

    changed base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from 'tailwindcss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/fylo-landing-page-with-two-column-layout',
+  base: '/fylo-landing-page',
   plugins: [react()],
   css: {
     postcss: {

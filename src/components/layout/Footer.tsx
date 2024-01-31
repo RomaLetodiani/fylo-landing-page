59a4a126f459b1851c3e86f355b51cbf12335979
RomaLetodiani <roman.letodiani03@gmail.com>
2024-02-01 00:13:50 +0400

    fixed socials links

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -28,10 +28,10 @@ const Footer = () => {
           ))}
           <div className="flex gap-3">
             {socials.map((item, index) => (
-              <a key={index}>
+              <a key={index} href={item.link}>
                 <div className="cursor-pointer border border-white rounded-full p-3">
                   <img
-                    className="invert brightness-0"
+                    className="invert brightness-0 w-6 h-6"
                     src={item.icon}
                     alt={item.alt}
                   />

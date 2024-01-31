02c725b94ee13ce278380a51dd1cb1f77a770821
RomaLetodiani <roman.letodiani03@gmail.com>
2024-01-31 22:48:36 +0400

    built testimonial component

--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -1,8 +1,21 @@
+import { QuoteIcon } from './shared/icons';
+
 type Props = { name: string; desc: string; image: string; text: string };
 
 const Testimonial = ({ name, desc, image, text }: Props) => {
-  console.log(name, desc, image, text);
-  return <div>Testimonial</div>;
+  return (
+    <div className="flex text-left flex-col gap-3 max-w-md bg-white p-10 rounded-lg shadow-lg">
+      <QuoteIcon />
+      <p className="font-medium">{text}</p>
+      <div className="flex items-center">
+        <img className="rounded-full w-12" src={image} alt="profile image" />
+        <div className="ml-5">
+          <h5 className="text-very-dark-blue font-bold text-xl">{name}</h5>
+          <p className="text-very-dark-blue text-sm">{desc}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Testimonial;
